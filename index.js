992affae597cbbbe6072d0e30910727aa1fbc3e3
Joseph <indiekaj@gmail.com>
2024-06-09 23:34:32 +0300

    put and get

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const  express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/product.model.js')
 const app = express()
+
+app.use(express.json())
 
 
 
@@ -8,6 +11,25 @@ const app = express()
 
 app.get('/', (req, res) =>{
     res.send("Hello from node api new one")
+});
+
+app.post('/api/products', async (req, res) => {
+    try {
+       const product =  await Product.create(req.body)
+       res.status(201).json(product)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+
+});
+
+app.get('/api/products/', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 });
 
 mongoose.connect("mongodb+srv://jose:KTGWIGJLzkSacGvA@food.bcrens6.mongodb.net/Node-API?retryWrites=true&w=majority&appName=food")
---
+++ models/product.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Please enter the quantity of the product"],
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "Please enter the price of the product"],
+      default: 0,
+    },
+    imageUrl: {
+      type: String,
+      required: false,
+    },
+  },
+  //two more fields for createdAt and updatedAt
+  {
+    timestamps: true,
+  }
+);
+
+const Product= mongoose.model("Product", ProductSchema);
+
+module.exports = Product;

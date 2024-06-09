518ace2a760ceb76556a0a73df2f8c1475d29125
Joseph <indiekaj@gmail.com>
2024-06-09 22:58:29 +0300

    database connection

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const  express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.listen(3000, ()=>{
-    console.log("Server is running in port 3000");
-})
+
 
 //insomnia === postman
 
 app.get('/', (req, res) =>{
-    res.send("Hello from node api")
+    res.send("Hello from node api new one")
 });
+
+mongoose.connect("mongodb+srv://jose:KTGWIGJLzkSacGvA@food.bcrens6.mongodb.net/Node-API?retryWrites=true&w=majority&appName=food")
+.then(()=>{
+    console.log("Connected to the database");
+    app.listen(3000, ()=>{
+        console.log("Server is running in port 3000");
+    })
+})
+.catch(()=>{
+    console.log("Connection failed");
+})
